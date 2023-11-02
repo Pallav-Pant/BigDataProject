1d9f90916b3c3866cb9d9b88cf5d3644f554defa
Pallav Pant <90005771+Pallav-Pant@users.noreply.github.com>
2023-11-01 23:25:00 -0400

    Added initial files

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # BigDataProject
  Project for Physics 5680
+
+# Packages being used
+    Gymnasium
+    MatplotLib
+    Numpy
+
+# Other Installations
+    C++ Build tools
+
+
+# Goals
+    Start by getting a self-learning Taxi Bot.
---
+++ Taxi/Taxi_Main.py
@@ -0,0 +1,45 @@
+
+# %%
+"""
+Taxi with Reinforcement Learning
+================================
+
+"""
+# Authors: Pallav Pant, Josh Lahr, Shannon Flaherty
+
+# %%
+# Imports and Environments
+import Taxi_Functions as tfunc
+import random
+import gymnasium as gym
+
+# %%
+# Taxi_Run_Test
+# Checks if the taxi environment runs correctly.
+render_mode = None
+test_env = gym.make("Taxi-v3", render_mode = render_mode)
+
+
+episodes = 1000
+outcomes = []
+for episode in range(episodes):
+    state = test_env.reset()
+    terminated, truncated = False, False
+    score = 0
+    while not (terminated or score < -300):
+        action = test_env.action_space.sample()
+        obs, reward, terminated, truncated, info = test_env.step(action)
+        score += reward
+
+#        print(f"Epsiode: {episode}, Score: {score}, Observation:{obs}")
+    outcomes.append(score)
+#    print("End of Episode\n")
+
+test_env.close()
+outcomes.sort()
+outcomes.reverse()
+print(outcomes)
+# %%
+# Actual Program Start
+if __name__ == '__main__':
+    print("Start")
