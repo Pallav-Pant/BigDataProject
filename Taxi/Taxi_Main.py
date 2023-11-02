a1f470cbcfc6dc3963709b77de69d746a120fbf1
Pallav Pant <90005771+Pallav-Pant@users.noreply.github.com>
2023-11-01 23:41:00 -0400

    Add Ideas file & comment some code

--- Taxi/Taxi_Main.py
+++ Taxi/Taxi_Main.py
@@ -10,35 +10,38 @@ Taxi with Reinforcement Learning
 # %%
 # Imports and Environments
 import Taxi_Functions as tfunc
-import random
 import gymnasium as gym
 
 # %%
 # Taxi_Run_Test
 # Checks if the taxi environment runs correctly.
-render_mode = None
-test_env = gym.make("Taxi-v3", render_mode = render_mode)
+
+#Render Mode = "human" allows us to see the taxi game being played.
+# render_mode = None
+# test_env = gym.make("Taxi-v3", render_mode = render_mode)
 
 
-episodes = 1000
-outcomes = []
-for episode in range(episodes):
-    state = test_env.reset()
-    terminated, truncated = False, False
-    score = 0
-    while not (terminated or score < -300):
-        action = test_env.action_space.sample()
-        obs, reward, terminated, truncated, info = test_env.step(action)
-        score += reward
+# episodes = 1000
+# outcomes = []
+# for episode in range(episodes):
+#     state = test_env.reset()
+#     terminated, truncated = False, False
+#     score = 0
+#     while not (terminated or score < -300):
+#         action = test_env.action_space.sample()
+#         obs, reward, terminated, truncated, info = test_env.step(action)
+#         score += reward
 
-#        print(f"Epsiode: {episode}, Score: {score}, Observation:{obs}")
-    outcomes.append(score)
-#    print("End of Episode\n")
+#         print(f"Epsiode: {episode}, Score: {score}, Observation:{obs}")
+#     outcomes.append(score)
+#     print("End of Episode\n")
 
-test_env.close()
-outcomes.sort()
-outcomes.reverse()
-print(outcomes)
+# test_env.close()
+# outcomes.sort()
+# outcomes.reverse()
+# print(outcomes)
+
+
 # %%
 # Actual Program Start
 if __name__ == '__main__':
