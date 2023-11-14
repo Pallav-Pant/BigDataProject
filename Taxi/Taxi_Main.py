49426912f07d552834be210186728831a31827e7
Pallav Pant <90005771+Pallav-Pant@users.noreply.github.com>
2023-11-13 22:21:05 -0500

    Removed Plotter and added it to tester

--- Taxi/Taxi_Main.py
+++ Taxi/Taxi_Main.py
@@ -3,17 +3,18 @@ import Taxi_Functions as tf
 import gymnasium as gym
 from tqdm import tqdm
 
-# %% Defining Hyperparameters for Training
+# %% Defining Hyperparameters and some constants for Training
 learning_rate = 0.05
 n_episodes = 10_000
 start_epsilon = 1.0
 epsilon_decay = start_epsilon / (n_episodes / 2) 
 final_epsilon = 0.1
 
-FINAL_SAVE_NAME = 'Taxi\Runs\Final'
 N_DATA_POINTS = 25
-#  Training
+
+# %%  Training
 env = gym.make("Taxi-v3")
+env.metadata['render_fps'] = 120
 agent = tf.Taxi(
     env,    
     learning_rate=learning_rate,
@@ -23,7 +24,7 @@ agent = tf.Taxi(
 )
 
 env = gym.wrappers.RecordEpisodeStatistics(env, deque_size=n_episodes)
-for episode in tqdm(range(n_episodes)):
+for episode in tqdm(range(n_episodes + 1)):
     obs, _ = env.reset()
     done = False
     score = 0
@@ -44,6 +45,5 @@ for episode in tqdm(range(n_episodes)):
         agent.save(f'Taxi\Runs\Generation_{episode}')
     agent.decay_epsilon()
 
-agent.save(FINAL_SAVE_NAME)
 
 
